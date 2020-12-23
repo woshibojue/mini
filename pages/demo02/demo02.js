85a419ba94d34797ad8f6b253aa899ab59997453
woshibojue@gmail.com <1053744861@qq.com>
2020-12-23 22:36:53 +0800

    git add

---
+++ pages/demo02/demo02.js
@@ -0,0 +1,28 @@
+// pages/demo02/demo02.js
+Page({
+
+  data: {
+
+    list2:[
+        {
+          id:2,
+          name:"aa"
+                
+
+        },
+        {
+          id:3,
+          name:"bb"
+                
+
+        },  {
+          id:4,
+          name:"cc"
+                
+
+        },
+    ]
+
+  }
+
+});
