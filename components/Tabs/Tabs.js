c7db2610573eaaf0ed2de05c503858d5a5591681
woshibojue@gmail.com <1053744861@qq.com>
2020-12-25 23:24:53 +0800

    git add

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,23 +1,51 @@
 // components/Tabs/Tabs.js
 Component({
   /**
-   * 组件的属性列表
+   * 里面存放的是 要从夫组件中接收的数据
    */
   properties: {
-
+    //要接收的数据的名称
+    // aaa: {
+    //   //type 要接收的数据的类型
+    //   type: String,
+    //   //value 默认值
+    //   value: "",
+    // },
+    tabs: {
+      type: Array,
+      value: [],
+    },
   },
 
   /**
    * 组件的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 组件的方法列表
    */
   methods: {
+    title_item_tap(e) {
+      console.log("佳儿");
 
-  }
-})
+      console.log(e.currentTarget.dataset.indexss);
+      // const{indexss}里面的indexss需要与自定义的data-indexss中的indexss一致
+      const { indexss } = e.currentTarget.dataset;
+      console.log({ indexss });
+      console.log(this.data);
+      //{tabs}需要与data中的tabs名字一致
+      let { tabs } = this.data;
+      //最严谨的做法：对tabs进行深拷贝
+      //let tabs = JSON.parse(JSON.stringify(this.data.tabs));
+      tabs.forEach((v, i) =>
+        i === indexss
+          ? (console.log(v), (v.isactive = true))
+          : (v.isactive = false)
+      );
+      this.setData({
+        tabs,
+      });
+    },
+  },
+});
--- pages/demo03/demo03.js
+++ pages/demo03/demo03.js
@@ -1,66 +1,70 @@
 // pages/demo03/demo03.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        name: "首页",
+        isactive: true,
+      },
+      {
+        id: 1,
+        name: "原创",
+        isactive: false,
+      },
+      {
+        id: 2,
+        name: "分类",
+        isactive: false,
+      },
+      {
+        id: 3,
+        name: "关于",
+        isactive: false,
+      },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
